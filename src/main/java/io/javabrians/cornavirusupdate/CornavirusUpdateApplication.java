2b6a20d86ba852ab145ecc53d290de7595aca614
aGlassOfWater <yliu2911@gmail.com>
2021-11-28 11:18:24 +0800

    Initial commit

---
+++ src/main/java/io/javabrians/cornavirusupdate/CornavirusUpdateApplication.java
@@ -0,0 +1,13 @@
+package io.javabrians.cornavirusupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornavirusUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornavirusUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrians/cornavirusupdate/CornavirusUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrians.cornavirusupdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornavirusUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
